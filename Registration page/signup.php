d60a52a85a743f2b29fa8cfb4188721839d62e11
ismail063 <ismailabdulqudus063@gmail.com>
2019-09-18 16:35:29 +0100

    i have worked on the back end again

--- Registration page/signup.php
+++ Registration page/signup.php
@@ -4,7 +4,11 @@ require_once('insert.php');
 
 if(!empty($_POST)){
   $insert->post($_POST);
-  echo '<p style="font-size: 2em; color: green; opacity: 0.7;">Post Successfully Published</p>';
+  header('location: ../dashboard.html');
+}
+
+else{
+  echo '<p style="font-size: 2em; color: purple; opacity: 0.7;">Please fill all the input fields</p>';
 }
 
 ?>
